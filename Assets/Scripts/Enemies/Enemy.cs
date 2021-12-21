aff589d0ae1b257014343be7dc63b20c902830eb
Toamig <carlosmiguelvieira@hotmail.com>
2021-12-21 11:38:03 +0000

    add golem model and enemy script

---
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int helthPoints;
+    public int attackDamage;
+    public float attackSpeed;
+    public int movementSpeed;
+    public int goldOnKill;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
