fd7dd30307d66e2783aaab846160d9b1e8448f97
Toamig <carlosmiguelvieira@hotmail.com>
2022-01-02 13:36:05 +0000

    create gameUI script

---
+++ Assets/Scripts/UserInterface/GameUI.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameUI : MonoBehaviour
+{
+    public GameObject healthBar;
+    public GameObject gold;
+    public GameObject rounds;
+    public GameObject speedUp;
+    public GameObject pauseGame;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
