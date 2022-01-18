81230a246914302d94bce55bc522cc459354c368
Christopher Abreu <up201604735@fe.up.pt>
2022-01-18 17:49:21 +0000

    Basic Files added

---
+++ Assets/Scripts/Sounds/AudioManager.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public SoundFile[] sounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Sounds/SoundFile.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+[System.Serializable]
+public class SoundFile
+{
+    public AudioClip clip;
+    public float volume;
+    public float pitch;
+
+}
