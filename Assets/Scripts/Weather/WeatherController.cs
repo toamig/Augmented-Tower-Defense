2611b72be9df0df49d958b33f79e908c105c4e4d
Toamig <carlosmiguelvieira@hotmail.com>
2022-01-19 15:15:04 +0000

    update weather particles

--- Assets/Scripts/Weather/WeatherController.cs
+++ Assets/Scripts/Weather/WeatherController.cs
@@ -10,12 +10,9 @@ public class WeatherController : MonoBehaviour
     public GameObject heavyClouds;
     public GameObject lightRain;
     public GameObject heavyRain;
-    public ParticleSystem[] rainParticles;
 
     private WeatherNetwork weatherNetwork;
     public WeatherNetwork.WeatherTypes weather = WeatherNetwork.WeatherTypes.Clear;
-    private ParticleSystem.EmissionModule rainEmission;
-    private ParticleSystem.MainModule rainColor;
 
     // Start is called before the first frame update
     void Awake()
@@ -33,7 +30,6 @@ public class WeatherController : MonoBehaviour
     }
     private void OnWeatherDataBroadcast(WeatherNetwork.WeatherTypes type)
     {
-        Debug.Log(type);
         weather = type;
     }
 
@@ -45,18 +41,12 @@ public class WeatherController : MonoBehaviour
         }
         else if (weather == WeatherNetwork.WeatherTypes.Clouds)
         {
-            lightClouds.SetActive(true);
             heavyClouds.SetActive(true);
         }
         else if (weather == WeatherNetwork.WeatherTypes.Drizzle)
         {
             lightClouds.SetActive(true);
             lightRain.SetActive(true);
-            foreach(ParticleSystem ps in rainParticles)
-            {
-                rainEmission = ps.emission;
-                rainEmission.rateOverTime = 5;
-            }
         }
         else if (weather == WeatherNetwork.WeatherTypes.FewClouds)
         {
@@ -64,65 +54,26 @@ public class WeatherController : MonoBehaviour
         }
         else if (weather == WeatherNetwork.WeatherTypes.HeavyRain)
         {
-            lightClouds.SetActive(true);
             heavyClouds.SetActive(true);
-            lightRain.SetActive(true);
             heavyRain.SetActive(true);
-            foreach (ParticleSystem ps in rainParticles)
-            {
-                rainEmission = ps.emission;
-                rainEmission.rateOverTime = 50;
-            }
         }
         else if (weather == WeatherNetwork.WeatherTypes.LightRain)
         {
             lightClouds.SetActive(true);
             lightRain.SetActive(true);
-            foreach (ParticleSystem ps in rainParticles)
-            {
-                rainEmission = ps.emission;
-                rainEmission.rateOverTime = 10;
-            }
         }
         else if (weather == WeatherNetwork.WeatherTypes.Rain)
         {
-            lightClouds.SetActive(true);
             heavyClouds.SetActive(true);
-            lightRain.SetActive(true);
             heavyRain.SetActive(true);
-            foreach (ParticleSystem ps in rainParticles)
-            {
-                rainEmission = ps.emission;
-                rainEmission.rateOverTime = 25;
-                rainColor = ps.main;
-                rainColor.startColor = Color.blue;
-            }
         }
         else if (weather == WeatherNetwork.WeatherTypes.Snow)
         {
-            lightClouds.SetActive(true);
-            heavyClouds.SetActive(true);
-            lightRain.SetActive(true);
-            heavyRain.SetActive(true);
-            foreach (ParticleSystem ps in rainParticles)
-            {
-                rainEmission = ps.emission;
-                rainEmission.rateOverTime = 5;
-                rainColor = ps.main;
-                rainColor.startColor = Color.white;
-            }
+            //TODO snow
         }
         else if (weather == WeatherNetwork.WeatherTypes.Strom)
         {
-            lightClouds.SetActive(true);
-            heavyClouds.SetActive(true);
-            lightRain.SetActive(true);
-            heavyRain.SetActive(true);
-            foreach (ParticleSystem ps in rainParticles)
-            {
-                rainEmission = ps.emission;
-                rainEmission.rateOverTime = 50;
-            }
+            //TODO storm
         }
         else
         {
